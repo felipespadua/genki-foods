e81214eb529e05a2ccf548c6f0cdcaee08e936de
marcellemcm <marcellemoreira@gmail.com>
2019-10-17 15:34:57 -0300

    Add view login, modify models and change css

--- models/User.js
+++ models/User.js
@@ -2,11 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    firstname: {
-        type: String,
-        required: true
-      },
-    lastname: {
+    name: {
         type: String,
         required: true
       },
