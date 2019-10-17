b5d10f303d2e860b26606b617027979223ef8b71
Felipe Padua <felipepadua@Mariannas-MBP.localdomain>
2019-10-17 11:35:05 -0300

    instalacao dos packages e inicio da implementacao da autenticacao por passport

---
+++ models/User.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema   = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: { type: String, required: true},
+  password: { type: String, required: true },
+  email: {type: String, required: true },
+  type: { type: String, enum: ['player', 'referee', 'admin'], default: 'player'},
+  matches: [{ type: Schema.Types.ObjectId, ref: "Match"}] ,
+  matchesOwner:  [{ type: Schema.Types.ObjectId, ref: "Match"}],
+  friends: [{ type: Schema.Types.ObjectId, ref: "User"}],
+  location: { type: { type: String }, coordinates: [Number], place: String},
+}, {
+  timestamps: true
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
