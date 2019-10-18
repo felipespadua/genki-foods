f8ad24e2f1c92a5cabb5e3d2a2d0d53f0d9498ca
Felipe Padua <felipepadua@Mariannas-MBP.localdomain>
2019-10-18 13:21:07 -0300

    ajuste na url de conexao para pegar base do heroku

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const flash = require("connect-flash");
 
 
 mongoose
-  .connect(process.env.MONGODB_URILOCAL, {useNewUrlParser: true})
+  .connect(process.env.MONGODB_URI, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
